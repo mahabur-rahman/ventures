7a87338faafe8c93e098630ea9e9ac6b4cbca95c
mahabur-rahman <annur4395@gmail.com>
2025-12-14 21:36:38 +0600

    feat/setup React Router and app routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,51 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Timeline from './pages/Timeline';
+import MurmurDetail from './pages/MurmurDetail';
+import UserProfile from './pages/UserProfile';
 
 function App() {
-
-
   return (
-    <>
- <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
-  )
+    <Router>
+      <AuthProvider>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Timeline />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/murmurs/:id"
+              element={
+                <ProtectedRoute>
+                  <MurmurDetail />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/users/:id"
+              element={
+                <ProtectedRoute>
+                  <UserProfile />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
